d6c7d02ceb1df761047f4d6c857af729779425bc
Walid Azzimani <walidazzimani@gmail.com>
2020-07-12 22:26:23 +0200

    groups de normalization

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,60 +12,67 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    /**
-     * L'encodeur de mots de passe
-     *
-     * @var UserPasswordEncoderInterface
-     */
-    private $encoder;
+ /**
+  * L'encodeur de mots de passe
+  *
+  * @var UserPasswordEncoderInterface
+  */
+ private $encoder;
 
-    public function __construct(UserPasswordEncoderInterface $encoder)
-    {
-        $this->encoder = $encoder;
+ public function __construct(UserPasswordEncoderInterface $encoder)
+ {
+  $this->encoder = $encoder;
+ }
+
+ public function load(ObjectManager $manager)
+ {
+  $faker = Factory::create('fr_FR');
+
+  for ($u = 0; $u < 10; $u++) {
+   $user = new User();
+
+   $chrono = 1;
+   $hash   = $this->encoder->encodePassword($user, "password");
+
+   $user->setFirstName($faker->firstName())
+    ->setLastName($faker->lastName)
+    ->setEmail($faker->email)
+    ->setPassword($hash);
+
+   $manager->persist($user);
+
+   for ($c = 0; $c < mt_rand(5, 20); $c++) {
+    $customer = new Customer();
+    $customer->setFirstName($faker->firstName())
+     ->setLastName($faker->lastName)
+     ->setCompany($faker->company)
+     ->setEmail($faker->email)
+     ->setUser($user);
+
+    $manager->persist($customer);
+
+    for ($i = 0; $i < mt_rand(3, 10); $i++) {
+     $invoice = new Invoice();
+     $invoice->setAmount($faker->randomFloat(2, 250, 5000))
+      ->setSentAt($faker->dateTimeBetween('-6 months'))
+      ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+      ->setCustomer($customer)
+      ->setChrono($chrono);
+
+     $chrono++;
+
+     $manager->persist($invoice);
     }
+   }
+  }
 
-    public function load(ObjectManager $manager)
-    {
-        $faker = Factory::create('fr_FR');
-
-        for ($u = 0; $u < 10; $u++) {
-            $user = new User();
-
-            $chrono = 1;
-            $hash = $this->encoder->encodePassword($user, "password");
-
-            $user->setFirstName($faker->firstName())
-                ->setLastName($faker->lastName)
-                ->setEmail($faker->email)
-                ->setPassword($hash);
-
-            $manager->persist($user);
-
-            for ($c = 0; $c < mt_rand(5, 20); $c++) {
-                $customer = new Customer();
-                $customer->setFirstName($faker->firstName())
-                    ->setLastName($faker->lastName)
-                    ->setCompany($faker->company)
-                    ->setEmail($faker->email)
-                    ->setUser($user);
-
-                $manager->persist($customer);
-
-                for ($i = 0; $i < mt_rand(3, 10); $i++) {
-                    $invoice = new Invoice();
-                    $invoice->setAmount($faker->randomFloat(2, 250, 5000))
-                        ->setSentAt($faker->dateTimeBetween('-6 months'))
-                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
-                        ->setCustomer($customer)
-                        ->setChrono($chrono);
-
-                    $chrono++;
-
-                    $manager->persist($invoice);
-                }
-            }
-        }
-
-        $manager->flush();
-    }
+  $manager->flush();
+ }
 }
+
+// -------------------------------- Aide-Mémoire ----------------------------
+// normalisation: objet -> tableau
+// serialisation: tableau -> Json
+
+//dénormalisation: tableau -> objet
+// déserialisation: Json -> tableau
--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -2,158 +2,167 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use App\Repository\CustomerRepository;
 use ApiPlatform\Core\Annotation\ApiFilter;
-use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
-use Doctrine\Common\Collections\ArrayCollection;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Repository\CustomerRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+//chapitre 49 -> à voir
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
- * @ApiResource()
+ * @ApiResource(normalizationContext={ "groups"={"curstomers_read" } })
+ *
  * @ApiFilter(SearchFilter::class, properties={"firstName":"partial","lastName","company"})
  * @ApiFilter(OrderFilter::class)
  */
 class Customer
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+ /**
+  * @ORM\Id()
+  * @ORM\GeneratedValue()
+  * @ORM\Column(type="integer")
+  */
+ private $id;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $firstName;
+ /**
+  * @ORM\Column(type="string", length=255)
+  * @Groups({"customers_read"})
+  */
+ private $firstName;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $lastName;
+ /**
+  * @ORM\Column(type="string", length=255)
+  * @Groups({"customers_read"})
+  */
+ private $lastName;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
+ /**
+  * @ORM\Column(type="string", length=255)
+  * @Groups({"customers_read"})
+  */
+ private $email;
 
-    /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $company;
+ /**
+  * @ORM\Column(type="string", length=255, nullable=true)
+  * @Groups({"customers_read"})
+  */
+ private $company;
 
-    /**
-     * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
-     */
-    private $invoices;
+ /**
+  * @ORM\OneToMany(targetEntity=Invoice::class, mappedBy="customer")
+  * @Groups({"customers_read"})
+  */
+ private $invoices;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
-     */
-    private $user;
+ /**
+  * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+  * @Groups({"customers_read"})
+  */
+ private $user;
 
-    public function __construct()
-    {
-        $this->invoices = new ArrayCollection();
-    }
+ public function __construct()
+ {
+  $this->invoices = new ArrayCollection();
+ }
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+ public function getId(): ?int
+ {
+  return $this->id;
+ }
 
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
+ public function getFirstName(): ?string
+ {
+  return $this->firstName;
+ }
 
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
+ public function setFirstName(string $firstName): self
+ {
+  $this->firstName = $firstName;
 
-        return $this;
-    }
+  return $this;
+ }
 
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
+ public function getLastName(): ?string
+ {
+  return $this->lastName;
+ }
 
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
+ public function setLastName(string $lastName): self
+ {
+  $this->lastName = $lastName;
 
-        return $this;
-    }
+  return $this;
+ }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+ public function getEmail(): ?string
+ {
+  return $this->email;
+ }
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+ public function setEmail(string $email): self
+ {
+  $this->email = $email;
 
-        return $this;
-    }
+  return $this;
+ }
 
-    public function getCompany(): ?string
-    {
-        return $this->company;
-    }
+ public function getCompany(): ?string
+ {
+  return $this->company;
+ }
 
-    public function setCompany(?string $company): self
-    {
-        $this->company = $company;
+ public function setCompany(?string $company): self
+ {
+  $this->company = $company;
 
-        return $this;
-    }
+  return $this;
+ }
 
-    /**
-     * @return Collection|Invoice[]
-     */
-    public function getInvoices(): Collection
-    {
-        return $this->invoices;
-    }
+ /**
+  * @return Collection|Invoice[]
+  */
+ public function getInvoices(): Collection
+ {
+  return $this->invoices;
+ }
 
-    public function addInvoice(Invoice $invoice): self
-    {
-        if (!$this->invoices->contains($invoice)) {
-            $this->invoices[] = $invoice;
-            $invoice->setCustomer($this);
-        }
+ public function addInvoice(Invoice $invoice): self
+ {
+  if (!$this->invoices->contains($invoice)) {
+   $this->invoices[] = $invoice;
+   $invoice->setCustomer($this);
+  }
 
-        return $this;
-    }
+  return $this;
+ }
 
-    public function removeInvoice(Invoice $invoice): self
-    {
-        if ($this->invoices->contains($invoice)) {
-            $this->invoices->removeElement($invoice);
-            // set the owning side to null (unless already changed)
-            if ($invoice->getCustomer() === $this) {
-                $invoice->setCustomer(null);
-            }
-        }
+ public function removeInvoice(Invoice $invoice): self
+ {
+  if ($this->invoices->contains($invoice)) {
+   $this->invoices->removeElement($invoice);
+   // set the owning side to null (unless already changed)
+   if ($invoice->getCustomer() === $this) {
+    $invoice->setCustomer(null);
+   }
+  }
 
-        return $this;
-    }
+  return $this;
+ }
 
-    public function getUser(): ?User
-    {
-        return $this->user;
-    }
+ public function getUser(): ?User
+ {
+  return $this->user;
+ }
 
-    public function setUser(?User $user): self
-    {
-        $this->user = $user;
+ public function setUser(?User $user): self
+ {
+  $this->user = $user;
 
-        return $this;
-    }
+  return $this;
+ }
 }
